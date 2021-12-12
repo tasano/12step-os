4c1ac0edc7f9ca457fe8421ba989433a090f5bea
tasano <13191864+tasano@users.noreply.github.com>
2021-12-12 18:15:42 +0900

    11th step

---
+++ src/11/os/main.c
@@ -0,0 +1,39 @@
+#include "defines.h"
+#include "interrupt.h"
+#include "kozos.h"
+#include "lib.h"
+
+/* システム・タスクとユーザ・スレッドの起動 */
+static int start_threads( int argc, char* argv[] )
+{
+    kz_run( test11_1_main, "test11_1", 1, 0x100, 0, NULL );
+    kz_run( test11_2_main, "test11_2", 2, 0x100, 0, NULL );
+
+    /* 優先順位を下げて、アイドルスレッドに移行する */
+    kz_chpri( 15 );
+
+    /* 割込み有効にする */
+    INTR_ENABLE;
+
+    while ( 1 ) {
+        /* 省電力モードに移行 */
+        asm volatile( "sleep" );
+    }
+
+    return 0;
+}
+
+int main( void )
+{
+    /* 割込み無効にする */
+    INTR_DISABLE;
+
+    puts( "kozos boot succeed!\n" );
+
+    /* OSの動作開始 */
+    kz_start( start_threads, "idle", 0, 0x100, 0, NULL );
+
+    /* ここには戻ってこない */
+
+    return 0;
+}
